e9d609a93c7b421320713fc33fff599eb06f68b2
Samoina <samoina.w@gmail.com>
2022-08-22 10:45:41 +0300

    Add a function to display the error if the number is outside the limits

--- app.js
+++ app.js
@@ -8,16 +8,31 @@
 //UI variables
 let minimum = parseInt(document.getElementById('minimum').innerHTML),
     maximum = parseInt(document.getElementById('maximum').innerHTML),
-    userInput = document.getElementById("user-input");
+    userInput = document.getElementById("user-input"),
+    gameDiv = document.getElementById('game');
 
 document.getElementById('submit-value').addEventListener('click', checkGuess);
 
-function checkGuess() {
+//function to show the error
+function showError(message){
+  let div = document.createElement('div');
+  div.style.backgroundColor = 'red';
+  div.style.margin = '3px';
+  div.innerHTML = `<p>${message}</p>`
+
+  gameDiv.parentNode.insertBefore(div, gameDiv);
+
+  setTimeout(() => {
+    div.style.display = 'none';
+  }, 2000);
+}
+
+function checkGuess(event) {
   if(userInput.value<minimum || userInput.value>maximum){
-    alert('Error! The number is outside the given range')
+    showError(`Your guess number,  ${userInput.value}, is outside the given range`)
   } else {
    alert(`Your guess was ${userInput.value}`);
   }
 
-  
+  event.preventDefault();
 }
